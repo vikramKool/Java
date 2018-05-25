ab1ec4210d7c0185a8fef42336ce5558d2b1044a
vikramKool <vikram.kadam@snapwiz.com>
2018-05-25 18:48:42 +0530

    Data  Types over

---
+++ src/languageFundamentals/$07_Data_Types_Boolean.java
@@ -0,0 +1,45 @@
+package languageFundamentals;
+
+public class $07_Data_Types_Boolean
+{
+    public static void main(String...args)
+    {
+        boolean a=true;
+
+        //boolean b=0; //CTE-->Incompatible types. Found: 'int', required: 'boolean'
+
+        //boolean c=True; //CTE-->Cannot resolve symbol 'True'
+                            //symbol:Variable True
+                            //location:Class $07_Data_Types_Boolean
+
+        //boolean d="true"; //CTE-->Incompatible types. Found: 'java.lang.String', required: 'boolean'
+
+        /*-------------------New Example-------------------
+
+
+
+        int x=0;
+
+        if (x) //CTE:- Incompatible types. Found: 'int', required: 'boolean'
+        {
+            System.out.print("Hello");
+        }
+        else
+        {
+            System.out.print("Hi");
+        }
+
+        */
+
+        /*-------------------New Example-------------------
+
+        while (1)//CTE:- Incompatible types. Found: 'int', required: 'boolean'
+        {
+            System.out.print("Hello");
+        }
+
+        */
+
+
+    }
+}
---
+++ src/languageFundamentals/$08_Data_Types_Char.java
@@ -0,0 +1,17 @@
+package languageFundamentals;
+
+public class $08_Data_Types_Char
+{
+    public static void main(String...args)
+    {
+        char ch01='a';
+        System.out.print(ch01);
+
+        //char ch02=null; //CTE:- Incompatible types. Found: 'null', required: 'char'
+
+
+
+
+
+    }
+}
